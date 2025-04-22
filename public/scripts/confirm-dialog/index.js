595ddca0466ecd5245f03160c4002824cb63d595
909back <909back@naver.com>
2025-04-23 03:14:22 +0900

    format: remove file

--- public/scripts/confirm-dialog/index.js
+++
@@ -1,7 +0,0 @@
-import ConfirmDialog from '../components/confirm-dialog.js';
-customElements.define('confirm-dialog', ConfirmDialog);
-const trigger = document.getElementById("trigger");
-const modal = document.querySelector('confirm-dialog');
-trigger?.addEventListener('click', () => {
-    modal.openModal();
-});
--- public/scripts/utils/contranst.js
+++
@@ -1,9 +0,0 @@
-export const router = [
-    {
-        href: '/confirm-dialog',
-        title: 'Confirm Dialog',
-        stylesheets: [
-            '/styles/css/confirm-dialog.css'
-        ]
-    }
-];
